1fd83fbf4ead451750cbe6500ecd875ed31b6dcc
JustinNazari <justinnazari@gmail.com>
2016-06-08 11:18:51 -0400

    changes to due page

--- app/models/league.rb
+++ app/models/league.rb
@@ -2,4 +2,14 @@ class League < ActiveRecord::Base
   belongs_to :commissioner
   has_many :managers
   has_many :dues
+
+  def amount_paid
+    if self.commissioner.due
+      paid = self.dues.select {|due| due.paid }.count
+      total = (self.commissioner.due.amount * paid)
+    else
+      total = 0
+    end
+    total
+  end
 end
