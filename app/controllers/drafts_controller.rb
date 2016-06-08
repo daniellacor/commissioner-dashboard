8cb50871e4d9ba5f3f9014335993a83773b60767
daniellacor <daniella.corricelli@gmail.com>
2016-06-08 11:00:17 -0400

    new draft calendar

--- app/controllers/drafts_controller.rb
+++ app/controllers/drafts_controller.rb
@@ -1,8 +1,13 @@
 class DraftsController < ApplicationController
 
+  include DraftHelper
+
   def index
     @league = current_user.leagues.first
     @headlines = Headlines.get_headlines
+
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today
+
   end
 
 
